46846f5b5f21c3b57391ba7e24512e2ba6216f12
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2023-01-22 23:03:13 +0800

    Update Build-A-Boat.lua

--- Games/Build-A-Boat.lua
+++ Games/Build-A-Boat.lua
@@ -1,7 +1,7 @@
 local hook
 hook = hookfunction(game.HttpGet,function(self,url,...)
     if tostring(url) == "https://raw.githubusercontent.com/StenDirt/Trash-Game/main/UI.lua" then
-        return hook(self,"https://raw.githubusercontent.com/VertigoCool99/Script/main/BuildABoat/BackupLib.lua",...)
+        return hook(self,"https://raw.githubusercontent.com/Ayanok0ji/Rvxploit/main/UILib/BABLib.lua",...)
     end
     return hook(self,url,...)
 end)
